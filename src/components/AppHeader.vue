62ab907387efd2f8426d1b26f8b6f45d0ff1718d
Luis Elenes <luiseelenes@outlook.com>
2022-11-26 18:27:30 -0700

    header y parte principal

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+        <div  id="tiraazul">
+            <span id="letter">Descuentos exclusivos en línea</span>
+        </div>
+        <div id="tiraaramarilla">
+            <img src="../assets/logo.jpg" class="imglogo">
+            <select name="select" class="buscar">
+                <option value="todo">Todo el sitio</option>
+                <option value="juguetes" selected>Juguetes</option>
+                <option value="hogarmuebles">Hogar y Muebles</option>
+            </select>
+                <input type="text" name="buscar" class="buscar" placeholder="Busca tus productos"/>
+            </div>
+    </header> 
+
+</template>
+
+<script>
+export default {
+  name: 'app-header',
+}
+</script>
+<style>
+    #letter{
+        color: white;
+       
+    }
+    #tiraazul{
+        background-color: #006fb9;
+        padding: 10px;
+        text-align: center;
+    }
+    #tiraaramarilla{
+        background-color: #FFDD2F;
+    }
+    .imglogo{
+        height: 40px;
+        width: 150px;
+        margin: 10px;
+        margin-left: 2% ;
+    }
+    .buscar{
+        padding-top: 15px;
+    }
+
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="principal"> 
+        <span class="textpri">Descubre lo más nuevo en moda para mujer </span>
+        <br>
+        <span class="textsec">Renueva tu guardarropa con los tonos, diseños y tendencias de la temporada otoño-invierno.</span>
+        <br>
+        <img src="../assets/logo.jpg" class="imgprincipal">
+    </div>
+</template>
+<script>
+export default {
+  name: 'app-home',
+}
+</script>
+<style>
+    #principal{
+        margin-left: 20%;
+        margin-top: 4%;
+        font-family:Sans-serif;
+    }
+    .textpri{
+        font-weight: bold;
+        font-size: 30px;
+    }
+
+    .textsec{
+        padding-top: 30px;
+    }
+    .imgprincipal{
+        margin-top: 10px;
+        height: 300px;
+        width: 1300px;
+        border-color: black;
+    }
+  
+</style>
